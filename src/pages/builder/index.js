1d5336c3622a6abb8f22d98e095bba21b82b2070
Anik720 <heanik39@gmail.com>
2023-07-31 03:25:24 +0600

    added authentication

--- src/pages/builder/index.js
+++ src/pages/builder/index.js
@@ -6,66 +6,82 @@ import Link from "next/link";
 import { useSelector } from "react-redux";
 import Image from "next/image";
 
+import intel from "../../../public/images/KaMA6u.jpg";
 const PcBuilder = ({}) => {
   const builder = useSelector((state) => state.builder);
   console.log(10, builder);
 
   const allCPU = builder?.products.filter((item) => item.category === "CPU");
-  const allMotherboard = builder?.products.filter((item) => item.category === "Motherboard");
+  const allMotherboard = builder?.products.filter(
+    (item) => item.category === "Motherboard"
+  );
   return (
     <div>
       <h1>builder</h1>
 
       <Card title="Build your pc">
         <Card type="inner" title="CPU">
-          <div className="flex justify-between bg-gray-500 p-5 mb-10" style={{borderRadius:"20px"}}>
-            <div ><h1 className="text-xl font-bold" >CPU</h1></div>
+          <div
+            className="flex justify-between bg-gray-500 p-5 mb-10"
+            style={{ borderRadius: "20px" }}
+          >
+            <div>
+              <h1 className="text-xl font-bold">CPU</h1>
+            </div>
             <div>
               <Link href="/builder/CPU">
-                <Button style={{color:"white"}}>Choose</Button>
+                <Button style={{ color: "white" }}>Choose</Button>
               </Link>
             </div>
           </div>
 
-          <div className="flex gap-5 ">
-          {allCPU.length &&
-            allCPU.map((item, index) => (
-              <div className="flex gap-20">
-                <div>
-                  <img src={item.image} width={100} height={100}/>
+          <div className="flex gap-5">
+            {allCPU.length > 0 &&
+              allCPU.map((item, index) => (
+                <div className="flex gap-20" key={index}>
+                  <div>
+                    <Image src={intel} width={100} height={100} />
+                  </div>
+                  <div>
+                    <h1>{item.productName}</h1>
+                  </div>
                 </div>
-                <div>
-                  <h1 key={index}>{item.productName}</h1>
-                </div>
-              </div>
-            ))}
+              ))}
           </div>
         </Card>
         <Card type="inner" title="Motherboard">
-          <div className="flex justify-between bg-gray-500 p-5 mb-10" style={{borderRadius:"20px"}}>
-            <div ><h1 className="text-xl font-bold" >Motherboard</h1></div>
+          <div
+            className="flex justify-between bg-gray-500 p-5 mb-10"
+            style={{ borderRadius: "20px" }}
+          >
+            <div>
+              <h1 className="text-xl font-bold">Motherboard</h1>
+            </div>
             <div>
               <Link href="/builder/Motherboard/Motherboard">
-                <Button style={{color:"white"}}>Choose</Button>
+                <Button style={{ color: "white" }}>Choose</Button>
               </Link>
             </div>
           </div>
 
-          <div className="flex gap-5 ">
-          {allMotherboard?.length &&
-            allMotherboard?.map((item, index) => (
-              <div className="flex gap-20">
-                <div>
-                  <img src={item.image} width={100} height={100}/>
+          <div className="flex gap-5">
+            {allMotherboard?.length ? (
+              allMotherboard.map((item, index) => (
+                <div className="flex gap-20" key={index}>
+                  <div>
+                    <Image src={intel} width={100} height={100} />
+                  </div>
+                  <div>
+                    <h1>{item.productName}</h1>
+                  </div>
                 </div>
-                <div>
-                  <h1 key={index}>{item.productName}</h1>
-                </div>
-              </div>
-            ))}
+              ))
+            ) : (
+              <p>No motherboards found.</p>
+            )}
           </div>
         </Card>
-        <Card type="inner" title="CPU">
+        {/* <Card type="inner" title="CPU">
           <div className="flex justify-between bg-gray-500 p-5 mb-10" style={{borderRadius:"20px"}}>
             <div ><h1 className="text-xl font-bold" >CPU</h1></div>
             <div>
@@ -160,7 +176,7 @@ const PcBuilder = ({}) => {
               </div>
             ))}
           </div>
-        </Card>
+        </Card> */}
       </Card>
     </div>
   );
